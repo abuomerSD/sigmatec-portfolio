d0d4345d03d97211b06eb899fb26b21167bd5b57
abuomersd <tayeb1293@yahoo.com>
2025-11-09 00:38:37 +0300

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <h1 class="text-4xl">شركة سيجماتك اتوميشن</h1>
+    <div>شركة سيجماتك</div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
